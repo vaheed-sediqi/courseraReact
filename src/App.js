b33f4b9513feb84bd129abd35cbdd32c6a170105
vaheed-sediqi <vaheed,sediqi@gmail.com>
2019-05-04 10:17:06 -0700

    Menu component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+       <div className="container">
+        <NavbarBrand href="#">Anakara Raki Balikyar</NavbarBrand>
+       </div>
+      </Navbar>
+
+      <Menu></Menu>
     </div>
   );
 }
+
+
 
 export default App;
